c825b065f2472711f52b794f15227a87655d3924
wechchile <wech@tie.cl>
2017-08-03 08:18:47 -0400

    se termino los java

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
-ob_alumno =[{
+var ob_alumno =[{
 	"codigo":"001",
 	"nombre":"Hernan",
-	"nota":97},
+	"nota":27},
 	{
 	"codigo":"002",
 	"nombre":"Hecto",
@@ -24,21 +24,77 @@ ob_alumno =[{
 	"nota":70},
 	{
 	"codigo":"007",
-	"nombre":"Hernan",
-	"nota":97},
-	{
-	"codigo":"007",
-	"nombre":"Hernan",
-	"nota":97},
+	"nombre":"Yocesli",
+	"nota":100},
 	{
 	"codigo":"008",
-	"nombre":"Hernan",
-	"nota":97},
+	"nombre":"Manuel",
+	"nota":60},
 	{
 	"codigo":"009",
-	"nombre":"Hernan",
-	"nota":97},
+	"nombre":"Oscar",
+	"nota":69},
 	{
 	"codigo":"010",
-	"nombre":"Hernan",
-	"nota":97}]
+	"nombre":"Maria",
+	"nota":57}];
+
+function lista_alumno(){
+	var i =0;
+	var l_html="";
+
+	for( i in ob_alumno){
+		l_html+="<tr><th>Codigo</th><th>Nombre</th><th>Nota</th></tr><tr><td>"+ob_alumno[i].codigo+"</td><td>"+ob_alumno[i].nombre+"</td><td>"+ob_alumno[i].nota+"<td></tr>";
+		//alert(ob_alumno[i].codigo);
+		
+	}
+	//alert(l_html);
+	document.getElementById("tabla").innerHTML=l_html;
+} 
+
+function prueba(){
+	var i=0;
+	for(i in ob_alumno){
+		alert(ob_alumno[i].codigo);
+	}
+}
+
+function buscar_mayor(){
+	var i=1;
+	var mayor="";
+	var valor=0;
+	for(i in ob_alumno){
+		if (ob_alumno[i].nota> valor){
+			valor= ob_alumno[i].nota;
+			mayor="<tr><th>Codigo</th><th>Nombre</th><th>Nota</th></tr><tr><td>"+ob_alumno[i].codigo+"</td><td>"+ob_alumno[i].nombre+"</td><td>"+ob_alumno[i].nota+"<td></tr>";
+		}
+			
+	}
+	document.getElementById("tabla").innerHTML=mayor;
+}
+
+function sacar_promedio(){
+	var i=0;
+	var conta=1;
+	var suma=0;
+	for(i in ob_alumno){
+		suma+=ob_alumno[i].nota;
+		conta++;
+	}
+	resulta=suma/conta;
+	document.getElementById("tabla").innerHTML="<tr><th>promedio de notas </th></tr><tr><td>"+resulta+"</td></tr>"
+}
+
+function buscar_menor(){
+	var i=0;
+	var menor='';
+	var valor=100;
+	for(i in ob_alumno){
+		if(valor>ob_alumno[i].nota){
+			valor=ob_alumno[i].nota;
+			menor="<tr><th>Codigo</th><th>Nombre</th><th>Nota</th></tr><tr><td>"+ob_alumno[i].codigo+"</td><td>"+ob_alumno[i].nombre+"</td><td>"+ob_alumno[i].nota+"<td></tr>";
+		}
+		alert(menor);
+	}
+	document.getElementById("tabla").innerHTML=menor;
+}
